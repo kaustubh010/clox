4e0fae64510f27a4585ed4ab334bc71784977c9c
Kaustubh <kaustubhbhara420@gmail.com>
2022-08-06 14:19:25 +0530

    few fixes

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,6 @@ export default function Home() {
       <Head>
         <title>Clox.com 100% Natural 0% Harmful</title>
         <meta name="description" content="Clox.com 100% Natural 0% Harmful" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <div className='z-0 pt-28 md:pt-20'>
         <Image src="/home.jpg" alt="" width={1500} height={800} />
