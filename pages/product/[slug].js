867da27d9965445da8feae9a3a6a60617e193f17
Kaustubh <kaustubhbhara420@gmail.com>
2022-08-09 19:24:51 +0530

    img

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -28,7 +28,7 @@ const Post = ({addToCart, product, clearCart, buynow}) => {
         <section className="text-gray-600 body-font overflow-hidden">
             <div className="container px-5 py-24 mx-auto">
                 <div className="lg:w-4/5 mx-auto flex flex-wrap justify-center items-center">
-                    <img alt="ecommerce" className="lg:w-[40vh] w-full lg:h-auto h-full object-cover object-center rounded" src={`${product.img}`} />
+                    <img alt="ecommerce" className="lg:w-[40vh] w-full lg:h-auto h-full object-cover object-center rounded" src={product.img} />
                     <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                         <h2 className="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
                         <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{product.title}</h1>
--- pages/shop.js
+++ pages/shop.js
@@ -13,7 +13,7 @@ const Shop = ({products}) => {
                         {products.map((item)=>{
                             return <Link key={item._id} href={`/product/${item.slug}`}><div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg mx-5 my-2">
                             <a className="block relative rounded overflow-hidden">
-                                <img alt="ecommerce" className="-z-10 relative m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src={`${item.img}`} />
+                                <img alt="ecommerce" className="-z-10 relative m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src={item.img} />
                             </a>
                             <div className="mt-4 text-center md:text-left">
                                 <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
